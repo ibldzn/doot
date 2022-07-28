267368477eaae4ca1e7c3b49d5ea04b0bb301eb8
ibldzn <51160226+ibldzn@users.noreply.github.com>
2022-07-28 14:00:45 +0700

    custom command

---
+++ config/nvim/lua/custom_commands.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+function M.setup()
+  local add_command = vim.api.nvim_create_user_command
+  add_command("CDC", "cd %:p:h", { desc = "Change directory to current file" })
+  add_command("CDP", "cd -", { desc = "Change to previous directory" })
+  add_command("EditConfig", "e $MYVIMRC | cd %:p:h", { desc = "Edit neovim configs" })
+end
+
+return M
