258b17d95a9711709cc412ad774c55c98aa79d7d
ibldzn <51160226+ibldzn@users.noreply.github.com>
2022-08-14 20:58:40 +0700

    colorizer toggle hotkey

--- config/nvim/lua/configs/colorizer.lua
+++ config/nvim/lua/configs/colorizer.lua
@@ -1,10 +1,16 @@
 local M = {}
 
 local colorizer = require("colorizer")
+local wk = require("which-key")
 
 function M.setup()
   colorizer.setup({})
-  vim.cmd("ColorizerToggle")
+  wk.register({
+    ["<leader>t"] = {
+      name = "Toggle",
+      ["C"] = { ":ColorizerToggle<cr>", "Colorizer" },
+    },
+  }, { mode = "n" })
 end
 
 return M
