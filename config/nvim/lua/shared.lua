c2fd6ecc3a374fc4069a6f749192ad8dc290f404
ibldzn <51160226+ibldzn@users.noreply.github.com>
2022-08-28 14:43:22 +0700

    new snapshot

--- config/nvim/lua/shared.lua
+++ config/nvim/lua/shared.lua
@@ -40,7 +40,7 @@ return {
     },
   },
   packer = {
-    snapshot_version = "v0.1.2",
+    snapshot_version = "v0.1.3",
     snapshot_path = util.join_paths(vim.fn.stdpath("config"), "snapshots"),
     compile_path = util.join_paths(vim.fn.stdpath("config"), "lua", "packer_compiled.lua"),
   },
