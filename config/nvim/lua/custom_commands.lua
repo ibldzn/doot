194c060e2b33abb71955e18202abccd880ce39ef
ibldzn <51160226+ibldzn@users.noreply.github.com>
2022-09-05 21:43:32 +0700

    custom commands

--- config/nvim/lua/custom_commands.lua
+++ config/nvim/lua/custom_commands.lua
@@ -5,6 +5,11 @@ function M.setup()
   add_command("CDC", "cd %:p:h", { desc = "Change directory to current file" })
   add_command("CDP", "cd -", { desc = "Change to previous directory" })
   add_command("EditConfig", "e $MYVIMRC | cd %:p:h", { desc = "Edit neovim configs" })
+  add_command(
+    "RemoveTrailingWhitespaces",
+    "%s/\\s\\+$//e",
+    { desc = "Remove trailing whitespaces" }
+  )
 end
 
 return M
