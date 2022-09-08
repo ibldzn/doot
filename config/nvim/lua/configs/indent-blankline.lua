3f7bd848d5dd642191af4e52c67ba2cd46b7e814
ibldzn <51160226+ibldzn@users.noreply.github.com>
2022-09-08 08:47:39 +0700

    disabled indentation color

--- config/nvim/lua/configs/indent-blankline.lua
+++ config/nvim/lua/configs/indent-blankline.lua
@@ -15,15 +15,6 @@ function M.setup()
       "mason",
       "packer",
     },
-    char_highlight_list = {
-      "IndentBlanklineIndent1",
-      "IndentBlanklineIndent2",
-      "IndentBlanklineIndent3",
-      "IndentBlanklineIndent4",
-      "IndentBlanklineIndent5",
-      "IndentBlanklineIndent6",
-    },
-    show_current_context = true,
   })
 end
 
