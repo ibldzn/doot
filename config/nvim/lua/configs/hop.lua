e992f417ca72fd7191fc19dc325042fe7e8270ce
ibldzn <51160226+ibldzn@users.noreply.github.com>
2022-09-13 18:28:31 +0700

    feat(hop): changed bindings

--- config/nvim/lua/configs/hop.lua
+++ config/nvim/lua/configs/hop.lua
@@ -1,48 +1,10 @@
 local M = {}
 
 local wk = require("which-key")
-local util = require("util")
 local hop = require("hop")
-local hop_hint = require("hop.hint")
 
 function M.setup()
   hop.setup()
-
-  wk.register({
-    ["f"] = {
-      util.wrap(
-        hop.hint_char1,
-        { direction = hop_hint.HintDirection.AFTER_CURSOR, current_line_only = true }
-      ),
-      "Find char 'forward-inclusive'",
-    },
-
-    ["F"] = {
-      util.wrap(
-        hop.hint_char1,
-        { direction = hop_hint.HintDirection.BEFORE_CURSOR, current_line_only = true }
-      ),
-      "Find char 'backward-inclusive'",
-    },
-
-    ["t"] = {
-      util.wrap(hop.hint_char1, {
-        direction = hop_hint.HintDirection.AFTER_CURSOR,
-        current_line_only = true,
-        hint_offset = -1,
-      }),
-      "Find char 'forward-exclusive'",
-    },
-
-    ["T"] = {
-      util.wrap(hop.hint_char1, {
-        direction = hop_hint.HintDirection.BEFORE_CURSOR,
-        current_line_only = true,
-        hint_offset = 1,
-      }),
-      "Find char 'backward-exclusive'",
-    },
-  })
 
   wk.register({
     ["s"] = {
@@ -52,6 +14,7 @@ function M.setup()
       ["w"] = { hop.hint_words, "Word" },
       ["l"] = { hop.hint_lines, "Lines" },
       ["s"] = { hop.hint_lines_skip_whitespace, "Line start" },
+      ["p"] = { hop.hint_patterns, "Pattern" },
     },
   })
 end
