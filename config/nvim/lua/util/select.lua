fb509903c3d1e7f2530220e32866c23cd8160003
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-01-15 22:21:33 +0700

    fix(nvim): vim.ui.select bug

--- config/nvim/lua/util/select.lua
+++ config/nvim/lua/util/select.lua
@@ -73,6 +73,7 @@ end
 function M.select(items, opts, on_choice)
   assert(items ~= nil and not vim.tbl_isempty(items), "No entries available.")
 
+  hide()
   vim.cmd.stopinsert()
 
   M.items = items
@@ -91,13 +92,16 @@ function M.select(items, opts, on_choice)
   vim.fn.search(vim.fn.expand("<cword>"), "bc")
   local new_pos = vim.api.nvim_win_get_cursor(0)
   vim.api.nvim_win_set_cursor(0, { old_pos[1], old_pos[2] })
-  local col = new_pos[2] - old_pos[2]
+  local col = 0
+  if new_pos[1] == old_pos[1] then
+    col = new_pos[2] - old_pos[2]
+  end
 
   -- create win
   local win_config = {
     relative = "cursor",
-    col = col,
-    row = 0,
+    col = col - 3,
+    row = 1,
     width = width,
     height = #items,
     style = "minimal",
