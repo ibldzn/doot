ae17173335c9e16db60a057350f03a3d2b4afc35
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-04-02 06:23:15 +0700

    feat: load ccc on keys

--- config/nvim/lua/plugins/ccc.lua
+++ config/nvim/lua/plugins/ccc.lua
@@ -17,47 +17,46 @@ local keys = {
 		"<leader>c",
 		desc = "Ccc",
 	},
+	{
+		"<leader>ct",
+		vim.cmd.CccHighlighterToggle,
+		desc = "Toggle",
+	},
+	{
+		"<leader>cp",
+		vim.cmd.CccPick,
+		desc = "Pick",
+	},
+	{
+		"<leader>cc",
+		vim.cmd.CccConvert,
+		desc = "Convert",
+	},
 }
 
-local config = function()
-	local ccc = require("ccc")
-	local wk = require("which-key")
-
-	ccc.setup({
-		highlighter = {
-			excludes = {
-				"NvimTree",
-				"alpha",
-				"dapui_breakpoints",
-				"dapui_hover",
-				"dapui_repl",
-				"dapui_scopes",
-				"dapui_stacks",
-				"help",
-				"lspinfo",
-				"mason",
-				"packer",
-			},
+local opts = {
+	highlighter = {
+		excludes = {
+			"NvimTree",
+			"alpha",
+			"dapui_breakpoints",
+			"dapui_hover",
+			"dapui_repl",
+			"dapui_scopes",
+			"dapui_stacks",
+			"help",
+			"lspinfo",
+			"mason",
+			"packer",
 		},
-		win_opts = {
-			border = shared.window.border,
-		},
-	})
-
-	vim.defer_fn(vim.cmd.CccHighlighterEnable, 0)
-
-	wk.register({
-		["<leader>c"] = {
-			name = "Ccc",
-			["t"] = { vim.cmd.CccHighlighterToggle, "Toggle" },
-			["p"] = { vim.cmd.CccPick, "Pick" },
-			["c"] = { vim.cmd.CccConvert, "Convert" },
-		},
-	})
-end
+	},
+	win_opts = {
+		border = shared.window.border,
+	},
+}
 
 M.cmd = cmd
 M.keys = keys
-M.config = config
+M.opts = opts
 
 return M
