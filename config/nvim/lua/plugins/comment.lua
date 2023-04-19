f1cb4936e2481ee056716e5f6fff2a70754b833a
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-04-19 15:58:27 +0700

    fix(nvim): fixed commentstring

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -9,44 +9,42 @@ local keys = {
 }
 
 local opts = {
-	{
-		toggler = {
-			line = "gcc",
-			block = "gbc",
-		},
-		opleader = {
-			line = "gc",
-			block = "gb",
-		},
-		extra = {
-			above = "gco",
-			below = "gcO",
-			eol = "gcA",
-		},
-		mappings = {
-			basic = true,
-			extra = true,
-		},
-		pre_hook = function(ctx)
-			local U = require("Comment.utils")
-
-			-- Determine whether to use linewise or blockwise commentstring
-			local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
-
-			-- Determine the location where to calculate commentstring from
-			local location = nil
-			if ctx.ctype == U.ctype.blockwise then
-				location = require("ts_context_commentstring.utils").get_cursor_location()
-			elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-				location = require("ts_context_commentstring.utils").get_visual_start_location()
-			end
-
-			return require("ts_context_commentstring.internal").calculate_commentstring({
-				key = type,
-				location = location,
-			})
-		end,
+	toggler = {
+		line = "gcc",
+		block = "gbc",
 	},
+	opleader = {
+		line = "gc",
+		block = "gb",
+	},
+	extra = {
+		above = "gco",
+		below = "gcO",
+		eol = "gcA",
+	},
+	mappings = {
+		basic = true,
+		extra = true,
+	},
+	pre_hook = function(ctx)
+		local U = require("Comment.utils")
+
+		-- Determine whether to use linewise or blockwise commentstring
+		local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
+
+		-- Determine the location where to calculate commentstring from
+		local location = nil
+		if ctx.ctype == U.ctype.blockwise then
+			location = require("ts_context_commentstring.utils").get_cursor_location()
+		elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+			location = require("ts_context_commentstring.utils").get_visual_start_location()
+		end
+
+		return require("ts_context_commentstring.internal").calculate_commentstring({
+			key = type,
+			location = location,
+		})
+	end,
 }
 
 M.keys = keys
