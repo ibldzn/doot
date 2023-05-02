2fe1189ac842994ef39288ba7ffb50775fbec7b5
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-02 10:23:14 +0700

    feat(nvim): automatically commit `lazy-lock.json` every 3 days

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -17,6 +17,39 @@ local lazy_bootstrap = function()
 	vim.opt.runtimepath:prepend(lazypath)
 end
 
+local get_latest_lazy_lock_commit_date = function()
+	local config_dir = vim.fn.stdpath("config")
+	local git_cmd = string.format("cd -P %s && git log -1 --format=%%cd --date=unix lazy-lock.json", config_dir)
+	local git_log = vim.fn.system(git_cmd)
+	local commit_date = vim.fn.substitute(git_log, "\n", "", "g")
+
+	return commit_date
+end
+
+local should_commit_lazy_lock = function()
+	local commit_date = get_latest_lazy_lock_commit_date()
+	local current_date = vim.fn.str2nr(vim.fn.strftime("%s"))
+
+	local diff = current_date - commit_date
+	local diff_in_days = diff / 60 / 60 / 24
+
+	return diff_in_days >= 3
+end
+
+local lazy_autocmds = function()
+	vim.api.nvim_create_autocmd("User", {
+		pattern = "LazyUpdate",
+		group = vim.api.nvim_create_augroup("CommitLazyLockJSON", { clear = true }),
+		callback = function()
+			if should_commit_lazy_lock() then
+				local config_dir = vim.fn.stdpath("config")
+				vim.cmd("silent! lcd " .. config_dir)
+				vim.cmd("silent! !git add lazy-lock.json && git commit -m 'chore(nvim): update `lazy-lock.json`'")
+			end
+		end,
+	})
+end
+
 local lazy_setup = function()
 	local lazy = require("lazy")
 
@@ -65,6 +98,7 @@ end
 local setup = function()
 	lazy_bootstrap()
 	lazy_setup()
+	lazy_autocmds()
 end
 
 M.setup = setup
