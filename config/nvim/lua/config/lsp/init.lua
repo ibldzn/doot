6c6fb8129c11a5f0ae537feb7c4fb1ac153a90c6
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-07-01 07:10:35 +0700

    chore(nvim): update inlay_hint api

--- config/nvim/lua/config/lsp/init.lua
+++ config/nvim/lua/config/lsp/init.lua
@@ -166,7 +166,7 @@ local on_attach = function(client, buf)
 	end
 
 	if client.supports_method("textDocument/inlayHint") and vim.fn.has("nvim-0.10") then
-		vim.lsp.buf.inlay_hint(buf, true)
+		vim.lsp.inlay_hint(buf, true)
 	end
 
 	if client.supports_method("textDocument/colorProvider") then
