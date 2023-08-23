9a21cbff49a62f917d479f77a52791200f3aa70b
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-08-23 07:58:17 +0700

    feat(nvim): added treesitter support for blade

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -60,6 +60,24 @@ local config = function()
 		"zig",
 	}
 
+	local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+	parser_config.blade = {
+		install_info = {
+			url = "https://github.com/EmranMR/tree-sitter-blade",
+			files = { "src/parser.c" },
+			branch = "main",
+		},
+		filetype = "blade",
+	}
+
+	vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+		group = vim.api.nvim_create_augroup("BladeFiltypeRelated", { clear = true }),
+		pattern = "*.blade.php",
+		callback = function()
+			vim.bo.filetype = "blade"
+		end,
+	})
+
 	ts_config.setup({
 		ensure_installed = filetypes,
 		incremental_selection = {
